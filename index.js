422bdfd44b642c0bfabdd9c2d48736127f521b54
Caleb Cochrane <Calebcochranebil33@gmail.com>
2023-06-14 11:05:47 -0700

    added log.txt to gitignore

---
+++ index.js
@@ -0,0 +1,93 @@
+/* importing the express package locally */
+const express = require("express");
+/* importing morgan */
+const morgan = require("morgan");
+/* declaring the variable 'app' and attached all functionalities of express to it */
+const app = express();
+
+const fs = require("fs");
+const path = require("path");
+
+const accessLogStream = fs.createWriteStream(path.join(__dirname, "log.txt"), {
+    flags: "a",
+});
+
+/* invoking morgan, instead of myLogger() function */
+app.use(morgan("common", { stream: accessLogStream }));
+
+/* allows the return of multiple static files in response to a request */
+app.use(express.static("public"));
+
+let myMovies = [
+    {
+        title: "2001: A Space Odyssey",
+        release: "1968",
+    },
+    {
+        title: "The Godfather",
+        release: "1972",
+    },
+    {
+        title: "Citizen Kane",
+        release: "1941",
+    },
+    {
+        title: "Raiders of the Lost Ark",
+        release: "1981",
+    },
+    {
+        title: "La Dolce Vita",
+        release: "1960",
+    },
+    {
+        title: "Seven Samuri",
+        release: "1954",
+    },
+    {
+        title: "In the Mood for Love",
+        release: "2000",
+    },
+    {
+        title: "There Will Be Blood",
+        release: "2007",
+    },
+    {
+        title: "Singin' in the Rain",
+        release: "1952",
+    },
+    {
+        title: "High Fidelity",
+        release: "2000",
+    },
+];
+
+/* GET requests */
+app.get("/movies", (req, res) => {
+    res.json(myMovies);
+});
+
+/* res.send object replaces response.writeHead and response.end code */
+app.get("/", (req, res) => {
+    res.send("Welcome to MyFlix Movie App!");
+});
+
+/* error handler comes after all route calls and app.use but before app.listen */
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send("It's not working right now!");
+});
+
+/* listen for requests, replaces http.createServer code */
+app.listen(8080, () => {
+    console.log("Your app is listening on port 8080.");
+});
+
+/* OLD CODE */
+// const http = require('http');
+
+// http.createServer((request, response) => {
+//   response.writeHead(200, {'Content-Type': 'text/plain'});
+//   response.end('Welcome to my book club!\n');
+// }).listen(8080);
+
+// console.log('My first Node test server is running on Port 8080.');
